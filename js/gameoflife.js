ddcb2d99b4b5c69e2e4d67d21cbef311f6868c70
Dany <danmfacto@gmail.com>
2021-11-15 20:34:08 +0100

    Complete task 8

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -31,10 +31,10 @@ const printCell = (cell, state) => {
 };
 
 const corners = (state = []) => {
-  let minX = Number.MAX_VALUE;
-  let minY = Number.MAX_VALUE;
-  let maxX = 0;
-  let maxY = 0;
+  let minX = state.length ? state[0][0] : 0;
+  let minY = state.length ? state[0][1] : 0;
+  let maxX = minX;
+  let maxY = minY;
   for (let i = 0; i < state.length; i++) {
     const element = state[i];
     const x = element[0];
@@ -52,22 +52,60 @@ const corners = (state = []) => {
       minY = y;
     }
   }
-  minX = minX === Number.MAX_VALUE ? 0 : minX;
-  minY = minY === Number.MAX_VALUE ? 0 : minX;
+
   return { topRight: [maxX, maxY], bottomLeft: [minX, minY] }
 };
 
-const printCells = (state) => { 
-  
+const printCells = (state) => {
+  let rec = corners(state);
+  let x = rec.topRight[0] - rec.bottomLeft[0] + 1;
+  const offSetX = rec.bottomLeft[0];
+  const offSetY = rec.bottomLeft[1];
+  let y = rec.topRight[1] - rec.bottomLeft[1] + 1;
+  let res = [];
+  for (let i = 0; i < x; i++) {
+    const line = []
+    for (let j = 0; j < y; j++) {
+      line.push(printCell([i + offSetX, j + offSetY], state));
+    }
+    if (line.length) {
+      res.push(line.join(" "));
+    }
+  }
+  //console.warn(res)
+  const rrr = res.join("\n") + (res.length === 1 ? "" : "\n")
+  return rrr;
 };
 
-const getNeighborsOf = ([x, y]) => { };
+const getNeighborsOf = ([x, y]) => {
+  let res = []
+  for (let i = x - 1; i <= x + 1; i++) {
+    for (let j = y - 1; j <= y + 1; j++) {
+      if (!(i === x && j === y)) {
+        res.push([i, j])
+      }
+    }
+  }
+  return res;
+};
+/**
+ * A revoir
+ * @param {*} cell 
+ * @param {*} state 
+ * @returns 
+ */
+const getLivingNeighbors = (cell, state) => {
+  const neb = getNeighborsOf(cell);
+  return neb.filter((cellNeb,) => {
+    return printCell(cellNeb, state) == "\u25A3";
+  })
+};
 
-const getLivingNeighbors = (cell, state) => { };
+const willBeAlive = (cell, state) => { 
 
-const willBeAlive = (cell, state) => { };
+};
 
-const calculateNext = (state) => { };
+const calculateNext = (statecls) => { };
 
 const iterate = (state, iterations) => { };
 
